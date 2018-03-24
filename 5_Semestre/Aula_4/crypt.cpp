cd6c648d46e5d87ea9c532355ce9bd58071f6fd2
Julio Cesar <julinn_cesar@hotmail.com>
2018-03-24 13:38:48 -0300

    aaa

--- 5_Semestre/Aula_4/crypt.cpp
+++ 5_Semestre/Aula_4/crypt.cpp
@@ -15,7 +15,6 @@ main(int argc, char *argv[]) {
 		printf("\ncript <arquivo> <senha>\n\n");
 	else {
 		ent = fopen(argv[1], "rb");							// rb pega binario; pq vai ler byte a byte
-		
 		if (ent == NULL)
 			printf("\n\nArquivo inexistente...\n");
 		else {
@@ -24,13 +23,9 @@ main(int argc, char *argv[]) {
 			if (arqsai[strlen(arqsai) - 1] == 'x')
 				arqsai[strlen(arqsai) - 2] = '\0';			//elimina o '.x';
 			else
-				strcat(arqsai, ".x");
+				strcat(arqsai, ".x");						//concatena as strings;					
 			
-//			strcat(arqsai, ".x");										//concatena as strings;
-			
-//			sai = fopen("saida.x", "wb");					//escrita binario
-			
-			sai = fopen(arqsai, "wb");
+			sai = fopen(arqsai, "wb");						//escrita binario
 			
 			byte = fgetc(ent);
 			while(!feof(ent)) {
